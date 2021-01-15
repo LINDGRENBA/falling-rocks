14fc1c49607592063cf30ef6afd12b6512dfd6b1
Brittany Lindgren <brittdevelopsit@gmail.com>
2021-01-14 17:50:42 -0800

    Set up initial component, confirm application runs.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <p>App working</p>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App;
